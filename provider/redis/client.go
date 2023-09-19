f47b908b107b9b69584306c8b4b20458aa9f30a6
Victor França Lopes <18534956+vflopes@users.noreply.github.com>
2023-09-19 16:50:23 -0300

    Add support to srv records for redis host

--- provider/redis/client.go
+++ provider/redis/client.go
@@ -1,6 +1,10 @@
 package redis
 
 import (
+	"fmt"
+	"log"
+	"net"
+
 	"github.com/protomesh/go-app"
 	"github.com/redis/go-redis/v9"
 )
@@ -9,7 +13,12 @@ type RedisDependency interface {
 }
 
 type RedisClient[D RedisDependency] struct {
-	Host   app.Config `config:"host,str" default:"localhost:6379" usage:"Redis host"`
+	*app.Injector[D]
+
+	Host app.Config `config:"host,str" default:"localhost:6379" usage:"Redis host"`
+
+	UseSrvRecords app.Config `config:"use.srv.records,bool" default:"false" usage:"Use SRV records to lookup Redis hosts"`
+
 	Client redis.UniversalClient
 }
 
@@ -17,7 +26,49 @@ func (rc *RedisClient[D]) Initialize() {
 
 	redisHost := rc.Host.StringVal()
 
-	if len(redisHost) > 0 {
-		rc.Client = redis.NewClient(&redis.Options{Addr: redisHost})
+	if len(redisHost) == 0 {
+		return
 	}
+
+	addrs := []string{}
+
+	if rc.UseSrvRecords.BoolVal() {
+
+		records, err := rc.lookupSrvRecords(redisHost)
+		if err != nil {
+			log.Panic("Failed to lookup SRV records", "error", err)
+		}
+
+		addrs = append(addrs, records...)
+
+	} else {
+
+		addrs = append(addrs, redisHost)
+
+	}
+
+	rc.Client = redis.NewUniversalClient(&redis.UniversalOptions{Addrs: addrs})
+}
+
+func (rc *RedisClient[D]) lookupSrvRecords(record string) ([]string, error) {
+
+	log := rc.Log().With("driver", "redis")
+
+	cname, srvs, err := net.LookupSRV("", "", record)
+	if err != nil {
+		log.Error("Failed to lookup SRV records", "error", err)
+		return nil, err
+	}
+
+	log.Debug("SRV records", "cname", cname, "srvs", srvs)
+
+	hosts := []string{}
+
+	for _, srv := range srvs {
+		log.Debug("SRV record", "srv", srv)
+		hosts = append(hosts, fmt.Sprintf("%s:%d", srv.Target, srv.Port))
+	}
+
+	return hosts, nil
+
 }
